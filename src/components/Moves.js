b08644dda4e912d27b6da798eca92605c87f2f08
Ryan Kubik <rmkubik@me.com>
2022-03-13 14:51:44 -0700

    Format Moves section

--- src/components/Moves.js
+++ src/components/Moves.js
@@ -2,17 +2,35 @@ import React from "react";
 import styled from "styled-components";
 import MovesList from "./MovesList";
 
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const RowItem = styled.div`
+  margin-right: 24px;
+`;
+
 const Moves = ({ currentMove, availableMoves, usedMoves, useReroll }) => {
   return (
     <>
       <h2>Moves</h2>
-      <h3>Current</h3>
-      <MovesList moves={currentMove ? [currentMove] : undefined} />
-      <button onClick={useReroll}>Reroll</button>
-      <h3>Available</h3>
-      <MovesList moves={availableMoves} />
-      <h3>Used</h3>
-      <MovesList moves={usedMoves} />
+      <Row>
+        <RowItem>
+          <h3>Current</h3>
+          <MovesList moves={currentMove ? [currentMove] : undefined} />
+          <button onClick={useReroll}>Reroll</button>
+        </RowItem>
+        <RowItem>
+          <h3>Available</h3>
+          <MovesList moves={availableMoves} />
+        </RowItem>
+        <RowItem>
+          <h3>Used</h3>
+          <MovesList moves={usedMoves} />
+        </RowItem>
+      </Row>
     </>
   );
 };
